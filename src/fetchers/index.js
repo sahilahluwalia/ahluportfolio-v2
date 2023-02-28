ae4975734ed65ffb11db2263fd2ce3fa3577d726
Sahil Ahluwalia <satrader2000@gmail.com>
2023-02-28 11:18:08 +0530

    fix: add jsonp requests

--- src/fetchers/index.js
+++ src/fetchers/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { decode as base64_decode, encode as base64_encode } from "base-64";
 
+import jsonp from "jsonp";
 const API_URL = process.env.REACT_APP_API_URL;
 const LOCAL_ENV = process.env.REACT_APP_DEVELOPEMENT_ENV;
 const contactForm = (payload) => {
@@ -12,14 +13,59 @@ const freeQuote = (payload) => {
 };
 
 const getIp = async () => {
-  const result = await axios.get("https://api.ipify.org?format=json");
-  return result.data.ip;
+  const result = await new Promise((resolve, reject) => {
+    jsonp("https://api.ipify.org?format=jsonp", null, (err, data) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      } else {
+        // console.log(data);
+        resolve(data);
+      }
+    });
+  });
+  // console.log(result.ip);
+  return result.ip;
 };
+
+// const getIp = async () => {
+//   const result = jsonp(
+//     "https://api.ipify.org?format=jsonp&callback=ip",
+//     null,
+//     (err, data) => {
+//       if (err) {
+//         console.error(err.message);
+//         return false;
+//       } else {
+//         console.log(data);
+//         return data;
+//       }
+//     }
+//   );
+//   return result;
+
+//   // const result = await axios.get("https://api.ipify.org?format=json");
+//   // return result.data.ip;
+// };
 
 const getIpData = async () => {
   const ip = await getIp();
-  const ipData = await axios.get(`https://ipapi.co/${ip}/json/`);
-  return ipData.data;
+  // console.log("my ip is ", ip);
+  const result = await new Promise((resolve, reject) => {
+    jsonp(`https://ipapi.co/${ip}/jsonp/`, null, (err, data) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      } else {
+        console.log(data);
+        resolve(data);
+      }
+    });
+  });
+  console.log("result is ", result);
+  return result;
+  // const ipData = await axios.get(`https://ipapi.co/${ip}/json/`);
+  // return ipData.data;
 };
 
 const locationName = (location) => {
@@ -34,7 +80,7 @@ const locationName = (location) => {
 };
 
 const currentUrlAndIpSender = async (ipData, location) => {
-  if (LOCAL_ENV == "local") {
+  if (LOCAL_ENV == "localy") {
     console.log("LOCAL DEV ENIRONMNET");
     return;
   }
@@ -46,6 +92,7 @@ const currentUrlAndIpSender = async (ipData, location) => {
   console.log(true);
   return result;
 };
+
 const ipSaver = (ipData) => {
   const now = new Date();
   // add ttl of 1 min to the data
@@ -59,15 +106,14 @@ const ipDataToLocalStorage = async (location) => {
   // check if ipData is in local storage
   const ipData = JSON.parse(localStorage.getItem("local"));
   // if ipData is not in local storage
-  // console.log(ipData);
 
   if (!ipData) {
     // get ip data
     // console.log("ip dont exist in browser");
     const ipData = await getIpData();
     // console.log("reqest is sent to get new ip data");
-
     // save ip data to local storage
+    // console.log("IP DATA IS");
     // console.log(ipData);
     ipSaver(ipData);
     // console.log("ip is saved");
