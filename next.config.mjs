f05c34565136e60d508c20f15862579730ddf594
Sahil Ahluwalia <satrader2000@gmail.com>
2024-08-24 14:14:58 +0530

    add: nextjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Outputs a Single-Page Application (SPA).
+    distDir: './build', // Changes the build output directory to `./dist`.
+    // import file location
+    // basePath:'/src',
+}
+
+export default nextConfig
